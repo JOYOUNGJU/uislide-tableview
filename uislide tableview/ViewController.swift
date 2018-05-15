5b71df9d170a176e79ab906fd418a730390d8521
youngju <asdadssad@naver.coim>
2018-05-15 15:32:20 +0900

    slide

--- uislide tableview/ViewController.swift
+++ uislide tableview/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var mytableview: UITableView!
+    @IBOutlet weak var mylb: UILabel!
+    @IBOutlet weak var myslide: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mytableview.dataSource = self
+        mytableview.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+       
     }
 
+    @IBAction func slide(_ sender: Any) {
+        mytableview.reloadData()
+        
+        let val = Int(myslide.value)
+        print(Int(myslide.value))
+        print("슬라이드 움직임")
+        mylb.text = String(val)
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identtifier = "RE"
+        let cell = mytableview.dequeueReusableCell(withIdentifier: identtifier, for: indexPath)
+        cell.textLabel?.text = String(Int(myslide.value) + indexPath.row)
+        return cell
+    }
 
+    
 }
 
